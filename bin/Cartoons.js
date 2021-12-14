ae9de54a66a618ac8edb0c98cff2fa3f10dbdbeb
SabaManjavidze <sabamanjavidze@gmail.com>
2021-12-15 00:08:03 +0400

    added cartoon directory specifycation with arguments

--- bin/Cartoons.js
+++ bin/Cartoons.js
@@ -1,12 +1,45 @@
 #! /usr/bin/env node
-const fs = require("fs")
-const {getVideoApi,getSlug,getPage,getMainApi}=require("./services")
-const yargs = require("yargs")
+
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const fs = require("fs")
+const {getVideoApi,getSlug,getPage,getMainApi}=require("./services")
+
+
+const yargs = require('yargs')
+const argv = yargs.argv
+const GivenTitle = argv._
+
+
+const cli_init =async ()=>{
+if(GivenTitle.length==0)
+    {
+        rl.question("Enter Cartoon Name : ",async (show_name)=>
+        {
+            const formated = show_name.replace(" ","+").toLowerCase()
+            const arr = await getSlug(SHOW_URL,formated)
+            displayOptions(arr)
+
+        })
+    }
+    else{
+        getWithYargs(argv.specify)
+    }
+}
+
+const getWithYargs = async (specify)=>{
+    let show_name=""
+
+    GivenTitle.map(child=>{
+        show_name+=(child+"+")
+    })
+    const arr = await getSlug(SHOW_URL,show_name.toLowerCase())
+    displayOptions(arr,specify)
+}
+
 
 const BASE_URL="https://app.opencdn.co/cartoon?id="
 const VIDEO_URL = "https://animepl.xyz/api/source/"
@@ -16,10 +49,11 @@ const EPISODE_URL="https://kisscartoon.city/movie/"
 
 var MaxEpisodes = 1;
 var Slug = ""
-var Path = ""
+var Path = "D:/My Documents/Cartoons/cartoon.json"
 
 let ep = 1
 const arr = []
+
 const fetchEpisode=async ()=>{
     const html = await getPage(EPISODE_URL,Slug,ep)
     const {max,semi} = await getMainApi(MaxEpisodes,html,BASE_URL)
@@ -27,24 +61,21 @@ const fetchEpisode=async ()=>{
     const {video,quality} = await getVideoApi(VIDEO_URL,semi)
     return {episode:MaxEpisodes-ep+1,url:video,quality:quality}
 }
-const displayOptions = (arr)=>{
+const displayOptions = (arr,specify)=>{
     rl.question("Choose Index : ",(index)=>{
 
         const splat = arr[index].url.split("/")
         Slug = splat[splat.length-2]
 
         console.log("Starting To Fetch Every Episode... \n")
-
-        rl.question("Enter File Or Folder Path (file must be .json) : ",(path)=>{
-            console.log(path=="")
-            if (path==""||path==null||path.includes(" ")) {
-                console.log(path)
-                Path = "D:/desktop/cartoon.json"
-            }else{
+        if(specify){
+            rl.question("Enter File Or Folder Path (file must be .json) : ",(path)=>{
                 Path=path
-            }
+                fetchAllTheEpisodes()
+            })
+        }else{
             fetchAllTheEpisodes()
-        })
+        }   
     })
 }
 const fetchAllTheEpisodes=async ()=>
@@ -57,6 +88,11 @@ const fetchAllTheEpisodes=async ()=>
         console.log(`Episode ${ep} Fetched \n`)
         ep++
         if(ep>MaxEpisodes){
+            const path_arr = Path.split("/")
+            const path_dir=Path.replace(path_arr[path_arr.length-1],"")
+            if (!fs.existsSync(path_dir)) {
+               fs.mkdirSync(path_dir,{recursive:true}) 
+            }
             fs.writeFileSync(Path,JSON.stringify(arr,null,2),{flag:"w+"},(err)=>{err&&console.log(err);return})
             console.log("Done!")
             return;
@@ -69,30 +105,6 @@ const fetchAllTheEpisodes=async ()=>
     ,2500
     )
 }
-const getWithYargs = async () =>{
-    let show_name=""
-    const words = yargs.argv._
-    words.map(child=>{
-        show_name+=(child+"+")
-    })
-    const arr = await getSlug(SHOW_URL,show_name.toLowerCase())
-    displayOptions(arr)
-}
-const cli_init = ()=>{
-    if(yargs.argv._.length==0)
-    {
-        rl.question("Enter Cartoon Name : ",async (show_name)=>
-        {
-            const formated = show_name.replace(" ","+").toLowerCase()
-            const arr = await getSlug(SHOW_URL,formated)
-            displayOptions(arr)
-
-        })
-    }
-    else{
-        getWithYargs()
-    }
-}
-cli_init(Path,Slug,SHOW_URL)
+cli_init()
 
 
---
+++ test/test.js
@@ -0,0 +1,20 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+
+yargs(hideBin(process.argv))
+.command('serve [port]', 'start the server', (yargs) => {
+  return yargs
+    .positional('port', {
+      describe: 'port to bind on',
+      default: 5000
+    })
+}, (argv) => {
+  if (argv.verbose) console.info(`start server on :${argv.port}`)
+  serve(argv.port)
+})
+.option('verbose', {
+  alias: 'v',
+  type: 'boolean',
+  description: 'Run with verbose logging'
+})
+.parse()
